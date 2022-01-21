ce8bf85523340faa54c25d051404f2e060b3f976
Pah Reh <45471747+Plehlowla@users.noreply.github.com>
2022-01-21 13:18:22 -0600

    finished simple color flipper

--- app.js
+++ app.js
@@ -5,9 +5,16 @@ const color = document.querySelector('.color')
 
 btn.addEventListener('click', function() {
     // get random number between 0 - 3 colors[#]
-    const randomNumber = Math.floor(Math.random() * 3) + 1;
+    // const randomNumber = Math.floor(Math.random() * 3) + 1;
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
 
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
 
 })
+
+function getRandomNumber() {
+    // return Math.floor(Math.random() * colors.length);
+    return Math.floor(Math.random() * colors.length);
+}
