6bb56bf5ca9978d5a384e4a3bab047604b819ca7
Pah Reh <45471747+Plehlowla@users.noreply.github.com>
2022-01-21 13:11:38 -0600

    fixed ul not removing line

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-const colors = ['green', 'red', 'rbga(133,22,200)','#f15025']
+const colors = ['green', 'red', 'rgba(133,22,200)','#f15025']
+
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+btn.addEventListener('click', function() {
+    // get random number between 0 - 3 colors[#]
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+
+})
